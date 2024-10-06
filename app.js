f9492da5e87481fabe5168e1affcce45720079c9
koushikxahmed <koushikxahmed@gmail.com>
2024-10-06 19:16:13 +0600

    custom error and async wrapper added.

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const tasks = require('./routes/tasks');
 const app = express();
-const connectDB = require('./DB/connection')
-require('dotenv').config();
+require("dotenv").config();
+const connectDB = require("./DB/connection");
+const tasks = require("./routes/tasks");
+const notFound = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/errorHandlerMiddleware");
 
 // middlewares
-app.use(express.static('./public'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// router middleware
-app.use('/api/v1/tasks',tasks)
+// middleware
+app.use("/api/v1/tasks", tasks); // router
+app.use(notFound); // route exceptions
+app.use(errorHandlerMiddleware); // erorr handler next()
 
 const port = 3000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port,console.log('Server is on 3000'))
-    } catch (error) {
-       console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log("Server is on 3000"));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
-
-// app.use(/api/v1/tasks)
-// router.route('/').get().post()
-// router.route('/:id').get().post().patch().delete()
+start();
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,58 +1,47 @@
 const Task = require('../model/task')
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ Length: tasks.length, tasks });
-  } catch (error) {
-    res.send({ status: failed, msg: "value not found." });
+const asyncWrapper = require("../middlewares/async");
+const { createCustomError } = require("../errors/customError");
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ Length: tasks.length, tasks });
+});
+
+const createData = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const updateData = asyncWrapper(async (req, res, next) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskId}`));
   }
-};
-const createData = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error.errors.name });
+
+  res.status(200).json({ task: task });
+});
+
+const singleTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOne({ _id: taskId });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskId}`));
   }
-};
-const updateData = async (req, res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res.status(404).json({ msg: `No task found by ${taskId}` });
-    }
-    res.status(200).json({ task: task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  res.status(200).json({ task });
+});
+
+const deleteSingleTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskId });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskId}`));
   }
-};
-const singleTask = async (req, res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findOne({ _id: taskId });
-    if (!task) {
-      return res.status(404).json({ msg: `No task found by ${taskId}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-const deleteSingleTask = async (req, res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskId });
-    if (!task) {
-      return res.status(404).json({ msg: `No task found by ${taskId}` });
-    }
-    res.status(200).json({ msg: `${taskId} delete succesfull..` });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+  res.status(200).json({ msg: `${taskId} delete succesfull..` });
+});
 
 module.exports = {getAllTasks,createData,updateData,singleTask,deleteSingleTask}
